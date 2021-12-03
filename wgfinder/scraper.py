f9d82bffebdf626a5323d7f64b574633e6c65fa2
Yannik Queisler <dev.yannik.queisler@gmail.com>
2021-12-03 14:25:39 +0100

    Fix url
    Took 7 minutes

--- wgfinder/scraper.py
+++ wgfinder/scraper.py
@@ -7,22 +7,23 @@ from bs4 import BeautifulSoup
 
 from wgfinder.models import FlatAd
 
-WG_GESUCHT_SEARCH_URL = "https://www.wg-gesucht.de/wg-zimmer-in-Berlin.8.0.0.0.html?offer_filter=1&city_id=8&noDeact" \
-                        "=1&dTo=1648677600&categories%5B%5D=0&rent_types%5B%5D=2&sMin=18&rMax=600&wgSea=2&wgMnF=2" \
-                        "&wgMxT=4&wgArt%5B%5D=12&wgArt%5B%5D=1&wgArt%5B%5D=11&wgArt%5B%5D=19&wgArt%5B%5D=16&wgArt%5B" \
-                        "%5D=15&wgArt%5B%5D=7&wgArt%5B%5D=5&wgArt%5B%5D=13&wgArt%5B%5D=22"
+WG_GESUCHT_BASE_URL = "https://www.wg-gesucht.de"
+WG_GESUCHT_SEARCH_QUERY = "wg-zimmer-in-Berlin.8.0.0.0.html?offer_filter=1&city_id=8&noDeact" \
+                          "=1&dTo=1648677600&categories%5B%5D=0&rent_types%5B%5D=2&sMin=18&rMax=600&wgSea=2&wgMnF=2" \
+                          "&wgMxT=4&wgArt%5B%5D=12&wgArt%5B%5D=1&wgArt%5B%5D=11&wgArt%5B%5D=19&wgArt%5B%5D=16&wgArt" \
+                          "%5B%5D=15&wgArt%5B%5D=7&wgArt%5B%5D=5&wgArt%5B%5D=13&wgArt%5B%5D=22"
 log = logging.getLogger(__name__)
 scraped_flat_ads = set()
 
 
 def find_shared_flats() -> set[FlatAd]:
-    html = requests.get(WG_GESUCHT_SEARCH_URL).text
+    html = requests.get(f"{WG_GESUCHT_BASE_URL}/{WG_GESUCHT_SEARCH_QUERY}").text
     page = BeautifulSoup(html, features="html.parser")
     flat_ads = set()
     for row in page.table.tbody.find_all("tr"):
         cols = row.find_all("td")
         roommates = len(cols[1].a.span.find_all("img"))
-        url = cols[2].a["href"]
+        url = f'{WG_GESUCHT_BASE_URL}/{cols[2].a["href"]}'
         uploaded = datetime.strptime(cols[2].a.span.text.strip(), "%d.%m.%Y").date()
         rent = int(cols[3].a.span.b.text.strip().rstrip("€"))
         size = int(cols[4].a.span.text.strip()[:-2])
